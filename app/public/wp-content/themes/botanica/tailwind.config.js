1b115a68ed41d0abc9548fd6c599e2f7fbea1ebc
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2026-01-05 20:46:45 -0500

    Add fonts and weights to tailwind config

--- app/public/wp-content/themes/botanica/tailwind.config.js
+++ app/public/wp-content/themes/botanica/tailwind.config.js
@@ -11,7 +11,17 @@ module.exports = {
         "light-sea-green": "#35A59Eff",
         "burnt-rose": "#823B45ff",
         teal: "#258482ff",
-      }
+      },
+      fontFamily: {
+        libre: ["'Libre Baskerville'", "serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        lora: ["Lora", "serif"],
+        sacramento: ["Sacramento", "cursive"],
+      },
+      fontWeight: {
+        semibold: 600,
+        medium: 500,
+      },
     },
   },
   plugins: [],
